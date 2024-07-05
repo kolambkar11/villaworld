bef51de7e6501216404ac82545235e151c25e7c7
Ashutosh Kolambkar <kolambkarashutosh@gmail.com>
2024-07-05 21:09:16 +0530

    final changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import logo from "./assets/img/logo.png";
 import "./App.css";
+import Villadetailed from "./components/villadetailed/Villadetailed";
+import Villalist from "./components/villalist/Villalist";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/nav/Nav";
+import NotFound from "./components/notfound/Notfound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route
+            activeClassName="active_class"
+            exact
+            path="/"
+            element={<Villadetailed />}
+          ></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
+      <Villalist />
+    </>
   );
 }
 
